c6b64aacde1363e635334e7348e1801770216922
sanober riaz <sanober@sanober.me>
2017-12-12 02:57:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function getFirstSelector(selector){
 
-  var element = document.querySelectorAll(selector)
+  var element = document.getElementById(selector)
   return element;
+}
+
+function nestedTarget(){
+  
+}
+
+function inscreaseRankBy(n){
+  
+}
+
+function deepestChild(){
+  
 }
