3649e6b42f05c05f066899cc13bf50dc2b9c71ae
sanober riaz <sanober@sanober.me>
2017-12-12 03:14:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,25 @@ function nestedTarget(){
 }
 
 function inscreaseRankBy(n){
+  const rl = document.querySelector('.ranked-list')
+  for (var i = 0; i < rl.length; i++) {
+    let child = rl[i].children  
 
+    for (var j = 0; j < child.length; j++) {
+      child[j].innerHTML = parseInt(child[j].innerHTML) + n 
+      
+    }
+    
+  }
 }
 
 function deepestChild(){
+  let nd = document.getElementById('grand-node')
+  let nextnd = nd.children[0]
 
+  while (nextnd) {
+    nd = nextnd
+    nextnd =  nd.children[0]
+    
+  }
 }
