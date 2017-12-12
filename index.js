5c89ad1a9316d87a10e3561908ef65aa8a1bd7f9
sanober riaz <sanober@sanober.me>
2017-12-12 03:20:49 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function getFirstSelector(s){
+  return document.querySelector(s)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+
+
+function deepestChild(){
+  let nd = document.getElementById('grand-node')
+  let nextnd = nd.children[0]
+
+  while (nextnd) {
+    nd = nextnd
+    nextnd =  nd.children[0]
+  }
+
+  return nd
+}
+
+function increaseRankBy(n){
+  const rl = document.querySelectorAll('.ranked-list')
+
+  for (var i = 0; i < rl.length; i++) {
+    var child = rl[i].children
+
+    for (var j = 0; j < child.length; j++) {
+      child[j].innerHTML = parseInt(child[j].innerHTML) + n
+
+    }
+
+  }
+
+}
